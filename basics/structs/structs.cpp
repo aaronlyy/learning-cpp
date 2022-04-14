35ba8c63fe4d0f011c89fac5a688d1c5aad8e75f
aaronlyy <aaronlevican@gmail.com>
2022-04-14 23:56:40 +0200

    structs

--- basics/structs/structs.cpp
+++ basics/structs/structs.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
-struct UserData {
-    
+struct User {
+    char* name;
+    unsigned int age;
+    unsigned int id;
 };
 
 int main() {
+    User levi = {"levi", 20, 777};
+
+    std::cout << levi.name << "\n";
+    std::cout << levi.age << "\n";
+    std::cout << levi.id << "\n";
+
+    User hans;
+    hans.name = "hans";
+    hans.age = 69;
+    hans.id = 2;
+
+    std::cout << hans.name;
+
     return 0;
 }
