a5702bb8db1da9a88a42c4db606bf163f5c5a65f
aaronlyy <aaronlevican@gmail.com>
2022-04-14 11:36:53 +0200

    pointers

--- basics/pointer/basics.cpp
+++ basics/pointer/basics.cpp
@@ -2,15 +2,38 @@
 
 // &: reference operator
 // *: dereference operator
+// 
+// nullptr
 
 int main(){
     // create a variable of type int in stack
     int a = 13;
     std::cout << "value of a: " << a << "\n";
     std::cout << "address of a (reference): " << &a << "\n";
+
     // create a pointer
     int* p = &a;
+
     // print the value of the pointer
     std::cout << "value of p: " << p << "\n";
     std::cout << "value of *p (dereference): " << *p << "\n";
+
+    // allocate an int on the heap using a int* and the keyword new
+    int* np = new int;
+
+    // assign a value
+    *np = 34;
+    std::cout << *np << "\n"; // dereference pointer and print its value
+
+    // de-allocate int
+    delete np; // remember: this only deletes and frees the memory on the heap, the pointer still exists
+    std::cout << np << "\n";
+
+    // set the pointer to nullptr to prevent errors later in the code
+    np = nullptr;
+
+    // could be tested like this if needed
+    if (np == nullptr) {
+        std::cout << "Pointer is a nullptr\n";
+    }
 }
--- basics/pointer/call_by_reference.cpp
+++ basics/pointer/call_by_reference.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 
 // &: reference operator
+
 // takes a reference to a variable (no local copy)
 void change_value(int &n, int v) {
     n = v;
 }
 
+// takes a pointer reference (no local copy of pointer)
+void change_value_p(int* &n, int v) {
+    *n = v;
+}
+
 int main() {
     int number = 10;
+    int *np = &number;
+
     change_value(number, 20);
+    std::cout << "number: " << number << "\n";
+
+    change_value_p(np, 34);
     std::cout << "number: " << number << "\n";
 }
