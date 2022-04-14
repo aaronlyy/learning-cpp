a0e312b037fc00e0bb99f7b2858f923a3e8676ff
aaronlyy <aaronlevican@gmail.com>
2022-04-14 13:32:23 +0200

    idk anymore

--- basics/arrays/array_as_parameter.cpp
+++ basics/arrays/array_as_parameter.cpp
@@ -9,6 +9,8 @@ void loop_arr(int* arr, int size) {
 int main() {
     int* arr = new int[3]{1,2,3}; // heap array
     loop_arr(arr, 3);
+    delete[] arr;
+    arr = nullptr;
 
     int arr2[]{4,5,6}; // stack array
     loop_arr(arr2, 3);
