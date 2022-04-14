a1da96719f1638bc6503f0ffc846ca282e4245f0
aaronlyy <aaronlevican@gmail.com>
2022-04-15 00:11:57 +0200

    added enum classes (still needs some fixing)

---
+++ basics/enums/enum_class.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+// enum = enumeration
+// valuenames are uppercase
+// values are constant
+// values are integers
+
+// in an enum class, enums can share the same values names
+
+enum class ExitCode: unsigned char {
+    OK = 0,
+    WARNING = 1,
+    ERROR = 2,
+    UNKNOWN = 3
+};
+
+enum class TestCode: unsigned char {
+    OK = 0,
+    WARNING = 1,
+    ERROR = 2,
+    UNKNOWN = 3
+};
+
+// can be replaced by, values are given incremental from 0
+// enum ExitCode {
+//     OK,
+//     WARNING,
+//     ERROR,
+//     UNKNOWN
+// };
+
+int main() {
+    ExitCode code1 = ExitCode::ERROR;
+    TestCode code2 = TestCode::ERROR;
+}
