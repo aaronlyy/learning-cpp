b38c14317b56d9f46a21e5f27611a36a523e43a5
aaronlyy <aaronlevican@gmail.com>
2022-04-15 13:02:29 +0200

    edit

--- basics/structs/structs.cpp
+++ basics/structs/structs.cpp
@@ -6,6 +6,10 @@ struct User {
     unsigned int id;
 };
 
+void access_struct_using_pointer(User* user) {
+    std::cout << user->name << "\n"; // use -> for accessing attributes from a struct pointer
+}
+
 int main() {
     User levi = {"levi", 20, 777};
 
@@ -18,7 +22,13 @@ int main() {
     hans.age = 69;
     hans.id = 2;
 
-    std::cout << hans.name;
+    std::cout << hans.name << "\n";
+
+    // designated struct initalizer >= c++20
+    User nerf = {.name = "nerf", .age = 10, .id = 33};
+
+
+    access_struct_using_pointer(&nerf);
 
     return 0;
 }
